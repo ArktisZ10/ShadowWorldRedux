b43494f6db1b1ec42e5951a17f0bb47652b11095
Erik Börne <erik.borne@outlook.com>
2022-10-06 01:20:12 +0200

    Initial commit

---
+++ desktop/src/dev/bjorne/shadowworldredux/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package dev.bjorne.shadowworldredux;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import dev.bjorne.shadowworldredux.ShadowWorldRedux;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Shadow World Redux");
+		new Lwjgl3Application(new ShadowWorldRedux(), config);
+	}
+}
